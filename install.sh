8bb4da712993735d4c5d743b63bfcea3ac1eb461
Seangottarun <sean.wu20s@outlook.com>
2023-10-11 23:13:14 +0200

    Add tmux plugin manager TPM initialization to install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ echo '\nsource ~/.bashrc_personal' >> ~/.bashrc
 
 cp .pythonrc ~/.pythonrc
 cp .ripgreprc ~/.ripgreprc
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/.tmux.conf
+echo "Remember to hit <prefix> I to install TPM plugins
 cp .vimrc ~/.vimrc
