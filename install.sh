d4076a1fd52e2fb33c9b305d7337bedbd1ece1b1
Seangottarun <sean.wu20s@outlook.com>
2023-11-28 18:54:57 +0100

    Add ideavimrc

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/.tmux.conf
 echo "Remember to hit <prefix> I to install TPM plugins"
 cp .vimrc ~/.vimrc
+cp .ideavimrc ~/.ideavimrc
+
